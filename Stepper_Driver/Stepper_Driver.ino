fdbdb28a4a3945ad6789abf25dd71293fedf07b2
Matt Oslin <moslin@seas.upenn.edu>
2018-04-01 11:36:23 -0400

    switched to simpler implementation for sending steps over serial

--- Stepper_Driver/Stepper_Driver.ino
+++ Stepper_Driver/Stepper_Driver.ino
@@ -9,129 +9,68 @@
 // Will not work with v1 shields
 
 #include <Wire.h>
-#include <AccelStepper.h>
 #include <Adafruit_MotorShield.h>
 #include <string.h>
 
-//Adafruit_MotorShield AFMSbot(0x61); // Rightmost jumper closed
-Adafruit_MotorShield AFMStop(0x60); // Default address, no jumpers
+Adafruit_MotorShield AFMS(0x60); // Default address, no jumpers
 
 // Connect two steppers with 200 steps per revolution (1.8 degree)
 // to the top shield
-Adafruit_StepperMotor *myStepper1 = AFMStop.getStepper(200, 1);
-Adafruit_StepperMotor *myStepper2 = AFMStop.getStepper(200, 2);
-
-char serial_buffer[20];
-int serial_index = 0;
-
-// you can change these to DOUBLE or INTERLEAVE or MICROSTEP!
-// wrappers for the first motor!
-void forwardstep1() {  
-  myStepper1->onestep(FORWARD, MICROSTEP);
-}
-void backwardstep1() {  
-  myStepper1->onestep(BACKWARD, MICROSTEP);
-}
-// wrappers for the second motor!
-void forwardstep2() {  
-  myStepper2->onestep(FORWARD, MICROSTEP);
-}
-void backwardstep2() {  
-  myStepper2->onestep(BACKWARD, MICROSTEP);
-}
-//// wrappers for the third motor!
-//void forwardstep3() {  
-//  myStepper3->onestep(FORWARD, INTERLEAVE);
-//}
-//void backwardstep3() {  
-//  myStepper3->onestep(BACKWARD, INTERLEAVE);
-//}
-
-// Now we'll wrap the 3 steppers in an AccelStepper object
-AccelStepper stepper1(forwardstep1, backwardstep1);
-AccelStepper stepper2(forwardstep2, backwardstep2);
-//AccelStepper stepper3(forwardstep3, backwardstep3);
+Adafruit_StepperMotor *stepper1 = AFMS.getStepper(200, 1);
+Adafruit_StepperMotor *stepper2 = AFMS.getStepper(200, 2);
 
 void setup()
 {  
-
   Serial.begin(9600);
   Serial.println("Serial started");
-//  AFMSbot.begin(); // Start the bottom shield
-  AFMStop.begin(); // Start the top shield
+  AFMS.begin(); // Start the top shield
 
-  stepper1.setMaxSpeed(100000.0);
-  stepper1.setAcceleration(10000.0);
-  stepper2.setMaxSpeed(100000.0);
-  stepper2.setAcceleration(10000.0);
+  // Uneccessary when using onestep
+  //stepper1->setSpeed(1000);
+  //stepper2->setSpeed(2000);
 }
 
 void loop()
 {
-  stepper1.run();
-  stepper2.run();
   handleSerial();
 }
 
 void handleSerial() {
  while (Serial.available() > 0) {
    char incomingCharacter = Serial.read();
-   if(incomingCharacter == '\n'){
-    serial_buffer[serial_index] = NULL;
-    serial_index = 0;
-    parseSerial();
-   }else{
-    //Serial.println(incomingCharacter);
-    serial_buffer[serial_index] = incomingCharacter;
-    serial_index++;
+   switch(incomingCharacter){
+    case 'r':
+      stepper1->release();
+      stepper2->release();
+      Serial.println("Released");
+      break;
+    case 'a':
+      stepper1->onestep(FORWARD, DOUBLE);
+      break;
+    case 'd':
+      stepper1->onestep(BACKWARD, DOUBLE);
+      break;
+    case 'w':
+      stepper2->onestep(FORWARD, DOUBLE);
+      break;
+    case 's':
+      stepper2->onestep(BACKWARD, DOUBLE);
+      break;
+    case 'f':
+      stepper1->onestep(FORWARD, MICROSTEP);
+      break;
+    case 'h':
+      stepper1->onestep(BACKWARD, MICROSTEP);
+      break;
+    case 't':
+      stepper2->onestep(FORWARD, MICROSTEP);
+      break;
+    case 'g':
+      stepper2->onestep(BACKWARD, MICROSTEP);
+      break;
+
+    default:
+      break;
    }
  }
 }
-
-void parseSerial(){
-  Serial.print("I got: ");
-  char *token;
-  token = strtok(serial_buffer, " ");
-  String str(token);
-  Serial.println(str);
-  if(!strcmp(token,"m1_stop")){
-    Serial.println("Stopping motor1");
-    myStepper1->release();
-  }else if(!strcmp(token,"m1_set_speed")){
-    char *speed = strtok(NULL, " ");
-    if (speed == NULL)Serial.println("No speed given!");
-    stepper1.setSpeed(atof(speed));
-    Serial.print("Set motor1 speed: ");
-    Serial.println(stepper1.speed());
-  }else if(!strcmp(token,"m1_get_speed")){
-    Serial.print("Motor1 Speed: ");
-    Serial.println(stepper1.speed());
-  }else if(!strcmp(token, "m1_set_pos")){
-    char *pos = strtok(NULL, " ");
-    if (pos == NULL)Serial.println("No position given!");
-    stepper1.moveTo(atof(pos));
-    Serial.print("Set motor1 pos: ");
-    Serial.println(stepper1.targetPosition());
-  }else if(!strcmp(token,"m2_stop")){
-    Serial.println("Stopping motor2");
-    myStepper2->release();
-  }else if(!strcmp(token,"m2_set_speed")){
-    char *speed = strtok(NULL, " ");
-    if (speed == NULL)Serial.println("No speed given!");
-    stepper2.setSpeed(atof(speed));
-    Serial.print("Set motor2 speed: ");
-    Serial.println(stepper2.speed());
-  }else if(!strcmp(token,"m2_get_speed")){
-    Serial.print("Motor2 Speed: ");
-    Serial.println(stepper2.speed());
-  }else if(!strcmp(token, "m2_set_pos")){
-    char *pos = strtok(NULL, " ");
-    if (pos == NULL)Serial.println("No position given!");
-    stepper2.moveTo(atof(pos));
-    Serial.print("Set motor2 pos: ");
-    Serial.println(stepper2.targetPosition());
-  }else{
-    Serial.println("Unknown command");
-  }
-}
-
